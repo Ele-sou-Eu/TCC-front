9177e5c503f5a01760e5b784bc64ebcbcbff80da
Leo <leo.cubo3@gmail.com>
2021-11-25 19:34:53 -0300

    Implementar CSS na página de login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -26,11 +26,31 @@
     <label for="senha">Senha</label>
     <input type="password" name="senha" id="senha" required>
     <button @click="submit(document.getElementById('matricula').value, document.getElementById('senha').value)">Submeter</button>-->
-    <input type="text" v-model="matricula" required>
+    <!-- <input type="text" v-model="matricula" required>
     <input type="password" v-model="senha" required>
     <button @click="submit">Entrar</button>
-    <button @click="register">Registrar</button>
+    <button @click="register">Registrar</button> -->
+<body>
+  <div class="container">
+    <div class="header">
+      <h1>UFRN - Projeto level-up</h1>
+      <h2>login</h2>
+    </div>
+    <div class="main">
+        <span>
+          <i class="fa fa-user"></i>
+          <input type="text" v-model="matricula" required>
+        </span><br>
+        <span>
+          <i class="fa fa-lock"></i>
+          <input type="password" v-model="senha" required>
+          </span><br>
+          <button @click="submit">Entrar</button>
+          <button @click="register">Registrar</button>
   </div>
+</div>
+</body>
+</div>
 </template>
 
 <script>
@@ -74,5 +94,76 @@ export default {
 </script>
 
 <style scoped>
+body {
+	font-family: sans-serif;
+	overflow: hidden;
+}
 
+.container {
+	width: 380px;
+	margin:7% auto;
+	border-radius: 25px;
+	background-color: rgba(0, 0, 0, 0.25);
+	box-shadow: 0 0 17px #333;
+}
+
+.header {
+	text-align: center;
+	padding-top: 20px;
+}
+
+.header h1 {
+	color: #333;
+	font-size: 45px;
+	margin-bottom: 20px;
+}
+
+.main {
+	text-align: center;
+}
+
+.main input{
+  width: 300px;
+}
+
+.main input, button {
+	height: 40px;
+	border:none;
+	outline: none;
+	padding-left: 40px;
+	box-sizing: border-box;
+	font-size: 15px;
+	color: #333;
+	margin-bottom: 30px;
+}
+
+.main button {
+  width: 200px;
+	padding-left: 0;
+	background-color: #83acf1;
+	letter-spacing: 1px;
+	font-weight: bold;
+	margin-bottom: 20px;
+}
+
+.main button:hover {
+	box-shadow: 2px 2px 5px #555;
+	background-color: #7799d4;
+}
+.main input:hover {
+	box-shadow: 2px 2px 5px #555;
+	background-color: #ddd;
+}
+
+.main span {
+	position: relative;
+}
+
+.main i {
+	position: absolute;
+	left: 15px;
+	color: #333;
+	font-size: 16px;
+	top: 2px;
+}
 </style>
